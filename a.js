c7e94bca65c6dc0c1e5b3c2f3b0846c11d907a50
Tommy Cheng <tomcheng10@gmail.com>
2023-08-18 11:17:43 -0500

    combine ex1 and ex2

--- a.js
+++ a.js
@@ -1,4 +1,8 @@
-let fahr = 60;
-let celius = (fahr - 32) / 9 * 5;
-alert(fahr + "F '=\" " + celius + "C");
 console.log("Hello World!!!");
+
+function myFunction() {
+    let fahr = 60;
+    let celius = (fahr - 32) / 9 * 5;
+    document.getElementById("demo").innerHTML =
+	fahr + "F '=\" " + celius + "C";
+}
