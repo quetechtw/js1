9702d994d8d5f4708fc6cdf538c8a08d5be6ab43
Qutech.tw <quetech.tw@gmail.com>
2023-09-20 04:39:59 -0500

    a.js: start while loop

--- a.js
+++ a.js
@@ -1,8 +1,15 @@
 console.log("Hello World!!!");
 
 function myFunction() {
-    let fahr = 60;
-    let celius = (fahr - 32) / 9 * 5;
-    document.getElementById("demo").innerHTML =
-	fahr + "F '=\" " + celius + "C";
+    lower = 0; /* lower limit of temperatuire scale */
+    upper = 300; /* upper limit */
+    step = 20; /* step size */
+
+    let fahr = lower;
+    while (fahr <= upper) {
+        let celius = (fahr - 32) / 9 * 5;
+        document.getElementById("demo").innerHTML =
+	    fahr + "F '=\" " + celius + "C";
+        fahr = fahr + step;
+    }
 }
